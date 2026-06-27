8f3eb2ab2f88228fe06633eba472579c6dd24fad
cinnamontoasty <faiths.05huffier@icloud.com>
2026-06-27 16:30:16 -0700

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDD_cBFHm-oCx4IWrlv7ac5f3xrNCZYpvg",
+  authDomain: "myfantasticcookbook.firebaseapp.com",
+  projectId: "myfantasticcookbook",
+  storageBucket: "myfantasticcookbook.firebasestorage.app",
+  messagingSenderId: "378863949498",
+  appId: "1:378863949498:web:64ac7f8f080fb4ffb01483"
+};
